00811a7f2c3e1e3a4d2e4e1d2add1cc61b2b4be0
ivan <ivan@igc1.local>
2024-09-23 16:33:49 +0200

    @feat: Implementació de la funció de suma

--- src/calc.c
+++ src/calc.c
@@ -1,3 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 #include "calc.h"
+
+int suma(int a, int b) {
+    return a + b;
+}
+
--- src/calc.h
+++ src/calc.h
@@ -1,5 +1,6 @@
 #ifndef CALC_H
 #define CALC_H
 
+int suma(int a, int b);
 
 #endif // CALC_H
