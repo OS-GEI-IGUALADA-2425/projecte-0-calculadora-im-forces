3f7a5b863bd79b54ed2819a3163489625a9362b5
ivan <ivan@igc1.local>
2024-09-23 17:15:28 +0200

    @feat: Implementacion de las funciones restantes

--- src/calc.c
+++ src/calc.c
@@ -9,3 +9,19 @@ int suma(int a, int b) {
 int resta(int a, int b) {
     return a - b;
 }
+
+int multiplicacio(int a, int b) {
+    return a * b;
+}
+
+double divisio(double a, double b) {
+    return a / b;
+}
+
+double arrel(double a) {
+    return sqrt(a);
+}
+
+int potencia(int a, int b) {
+    return pow(a, b);
+}
--- src/calc.h
+++ src/calc.h
@@ -3,5 +3,9 @@
 
 int suma(int a, int b);
 int resta(int a, int b);
+int multiplicacio(int a, int b);
+double divisio(double a, double b);
+double arrel(double a);
+int potencia(int a, int b);
 
 #endif // CALC_H
